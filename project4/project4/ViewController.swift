e12e210d751c150c1bd4201050d1dc1a4e752c9f
yzhukovs <yzhukovs@bumnetworks.com>
2019-06-11 12:31:02 -0400

    added back and forward - project 4

--- project4/project4/ViewController.swift
+++ project4/project4/ViewController.swift
@@ -30,11 +30,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-        
+        let back = UIBarButtonItem(title: "<-", style: .done, target: webView, action: #selector(webView.goBack))
+        let forward = UIBarButtonItem(title: "->", style: .done, target: webView, action: #selector(webView.goForward))
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
         let progressButton = UIBarButtonItem(customView: progressView)
-        toolbarItems = [progressButton, spacer, refresh]
+        toolbarItems = [progressButton, spacer, refresh, back, forward]
         navigationController?.isToolbarHidden = false
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
