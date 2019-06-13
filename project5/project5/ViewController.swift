6ba5eda2cbe2163487b06aff94ab2689a36012e1
yzhukovs <yzhukovs@bumnetworks.com>
2019-06-13 14:08:12 -0400

    completed challenges for project 5

--- project5/project5/ViewController.swift
+++ project5/project5/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector (promptForAnswer))
         
-
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .play, target: self, action: #selector(startGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -30,7 +30,7 @@ class ViewController: UITableViewController {
         startGame()
     }
 
-    func startGame(){
+    @objc func startGame(){
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
