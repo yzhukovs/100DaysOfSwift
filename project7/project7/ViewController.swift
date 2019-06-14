bace89bf6e7e90abb6698a8a47a9b770e44b1d0f
yzhukovs <yzhukovs@bumnetworks.com>
2019-06-14 17:25:45 -0400

    set up project7

---
+++ project7/project7/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by Yvette Zhukovsky on 6/14/19.
+//  Copyright © 2019 Lambda School Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        petitions.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
+}
+
