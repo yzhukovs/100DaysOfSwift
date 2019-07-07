5fc930c8e4432b0f6a22cd520635123be70833ed
yzhukovs <yzhukovs@bumnetworks.com>
2019-07-07 00:38:42 -0400

    working on challenge7

--- Challenges/challenge7.playground/Contents.swift
+++ Challenges/challenge7.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var str = "Hello, playground"
+func Spaced(a: String)-> String {
+    var arrayChar = [Character]()
+    //create an array to store output
+    var spaces = [Character]()
+    //iterate through string
+    for (i, j) in Array(a).enumerated() {
+        //see if current and next item is space
+        if j == " " && Array(a)[i + 1] == " "{
+            
+        }
+    }
+    return String(spaces)
+}
+
+Spaced(a: "a  b    c")
