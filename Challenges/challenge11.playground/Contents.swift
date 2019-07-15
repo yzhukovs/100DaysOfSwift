ed2d0653d893015e22a51f7734667a7350615764
yzhukovs <yzhukovs@bumnetworks.com>
2019-07-15 14:16:00 -0400

    finished challenge 11

--- Challenges/challenge11.playground/Contents.swift
+++ Challenges/challenge11.playground/Contents.swift
@@ -1,30 +1,21 @@
 import UIKit
 
 func different3Letters(a: String, b: String) ->Bool {
-    var stringA = [Character]()
-    var stringB = [Character]()
-    stringA = Array(a)
-    stringB = Array(b)
+    let stringA = Array(a)
+    var stringB = Array(b)
     var count = 0
     if stringA.count != stringB.count { return false}
     for (i_a, j_a) in stringA.enumerated() {
-        for (i_b, j_b) in stringB.enumerated() {
-            if i_a != i_b {
-                return false
-            }
-            
-            if j_a != j_b {
+        if stringB[i_a] != j_a {
                 count += 1
-            }
-            if count > 3 {
+           
+            if count > 3{
                 return false
-            } else {
-                return true
                 }
-            
         }
-    }
-    return false
+        }
+    
+    return true
 }
 
-different3Letters(a: "Grans", b: "Cramp")
+different3Letters(a: "Clamp", b: "Cramp")
