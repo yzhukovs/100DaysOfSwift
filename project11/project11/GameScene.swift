aec3ab0f04a16a6ca4e1e2fd10917867f1384ec1
yzhukovs <yzhukovs@bumnetworks.com>
2019-07-19 23:58:09 -0400

    added bouncer

--- project11/project11/GameScene.swift
+++ project11/project11/GameScene.swift
@@ -20,6 +20,12 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = CGPoint(x: 512, y: 0)
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
        
         }
         
@@ -30,7 +36,7 @@ class GameScene: SKScene {
         
         let ball = SKSpriteNode(imageNamed: "ballRed")
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-        ball.physicsBody?.restitution = 0.4
+        ball.physicsBody?.restitution = 0.9
         ball.position = location
         addChild(ball)
 //        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
