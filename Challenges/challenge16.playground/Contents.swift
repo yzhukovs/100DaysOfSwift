1b1571f75fc40dd340bd6420add72fca0256392e
yzhukovs <yzhukovs@bumnetworks.com>
2019-07-26 15:13:19 -0400

    finished challenge16

--- Challenges/challenge16.playground/Contents.swift
+++ Challenges/challenge16.playground/Contents.swift
@@ -2,7 +2,22 @@ import UIKit
 
 func fizzBuzz(a: Int)-> String {
     
+    switch (a % 3 == 0, a % 5 == 0 ) {
+    case (true, true):
+        return "Fizz Buzz"
+      case (true, false):
+        return "Fizz"
+    case (false, true):
+        return "Buzz"
+    default:
+        return String(a)
+    }
     
     
 }
+
+fizzBuzz(a: 3)
+fizzBuzz(a: 5)
+fizzBuzz(a: 15)
+fizzBuzz(a: 1)
 
