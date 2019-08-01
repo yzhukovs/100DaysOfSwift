334c3be58269335b6e1cb976666089ee12914f7c
yzhukovs <yzhukovs@bumnetworks.com>
2019-08-01 19:41:43 -0400

    testing userDefaults

--- project12/project12/ViewController.swift
+++ project12/project12/ViewController.swift
@@ -16,6 +16,18 @@ class ViewController: UIViewController {
         defaults.set(25, forKey: "age")
         defaults.set(true, forKey: "userFaceID")
         defaults.set(CGFloat.pi, forKey: "Pi")
+        defaults.set("Yvette", forKey: "name")
+        defaults.set(Date(), forKey: "lastRun")
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        let dict = ["Name":"Yvette", "Country": "US"]
+        defaults.set(dict, forKey: "SavedDict")
+        
+        let savedInteger = defaults.integer(forKey: "age")
+        let savedBool = defaults.bool(forKey: "userFaceID")
+        
+        let savedArray = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        let savedDict = defaults.object(forKey: "SavedDict") as? [String: String] ?? [String: String]()
     }
 
 
