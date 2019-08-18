61a0b857597dd66ced87c74e83bb4f08a34067e5
yzhukovs <yzhukovs@bumnetworks.com>
2019-08-18 15:26:44 -0400

    set up project 14

---
+++ project14/project14/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  project14
+//
+//  Created by Yvette Zhukovsky on 8/18/19.
+//  Copyright © 2019 bumnetworks. All rights reserved.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+   
+    
+    override func didMove(to view: SKView) {
+        
+        // Get label node from scene and store it for use later
+       
+        }
+        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+}
