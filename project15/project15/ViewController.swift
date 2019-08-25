54b391871e6ee082a53daca8e53fc1a50c4aeeb3
yzhukovs <yzhukovs@bumnetworks.com>
2019-08-24 23:18:49 -0400

    added animate(withDuration:)

--- project15/project15/ViewController.swift
+++ project15/project15/ViewController.swift
@@ -20,7 +20,20 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
     }
 
-    @IBAction func tapped(_ sender: Any) {
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+            
+        }) { (finished) in
+            sender.isHidden = false
+        }
         currentAnimation += 1
         
         if currentAnimation > 7 {
