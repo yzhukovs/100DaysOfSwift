31021d3efbc0be689a2f0b31672499f4cab4be70
yzhukovs <yzhukovs@bumnetworks.com>
2019-09-13 11:00:01 -0400

    first challenge done - changed pinTintColor

--- project16/project16/ViewController.swift
+++ project16/project16/ViewController.swift
@@ -30,11 +30,12 @@ class ViewController: UIViewController, MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard annotation is Capital else {return nil}
         let identifier = "Capital"
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        
         if annotationView == nil {
             annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
             annotationView?.canShowCallout = true
-            
+            annotationView?.pinTintColor = .cyan
             let btn = UIButton(type: .detailDisclosure)
             annotationView?.rightCalloutAccessoryView = btn
         } else {
