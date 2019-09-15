10dbac16da8c5a0e791d95fc366e9b4b98539b51
yzhukovs <yzhukovs@bumnetworks.com>
2019-09-14 23:47:02 -0400

    project 17 set up

---
+++ project17/project17/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  project17
+//
+//  Created by Yvette Zhukovsky on 9/14/19.
+//  Copyright © 2019 bumnetworks. All rights reserved.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+   
+    
+    override func didMove(to view: SKView) {
+        
+       
+        }
+        
+    
+    override func update(_ currentTime: TimeInterval) {
+       
+    }
+}
