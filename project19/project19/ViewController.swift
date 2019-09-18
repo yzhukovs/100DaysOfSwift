0c83ea33389786e44726a528f9c718e7e01f83f7
yzhukovs <yzhukovs@bumnetworks.com>
2019-09-18 14:21:46 -0400

    project 19 setup

---
+++ project19/project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project19
+//
+//  Created by Yvette Zhukovsky on 9/18/19.
+//  Copyright © 2019 bumnetworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
