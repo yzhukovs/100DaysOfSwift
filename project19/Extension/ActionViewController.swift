95fd7db57e539e50419f1ebdc76398446a550dae
yzhukovs <yzhukovs@bumnetworks.com>
2019-09-18 21:22:38 -0400

    changed ActionViewController

---
+++ project19/Extension/ActionViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Yvette Zhukovsky on 9/18/19.
+//  Copyright © 2019 bumnetworks. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, errror) in
+                    
+                }
+            }
+        }
+        
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
