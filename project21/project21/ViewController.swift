aaece9533e6ed84f6b2ad7cbd77b3b63d26c6071
yzhukovs <yzhukovs@bumnetworks.com>
2019-10-18 16:03:15 -0400

    created notifications

--- project21/project21/ViewController.swift
+++ project21/project21/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -14,15 +15,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
 
     @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh!")
+            }
+        }
         
     }
 
     @objc func scheduleLocal(){
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        center.removeAllPendingNotificationRequests()
+        content.title = "Late wake up call"
+        content.body = "The early birds get catches the warm, but the second mouse gets the cheese"
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = .default
         
+        var dateComponents = DateComponents()
+        dateComponents.hour  = 10
+        dateComponents.minute = 30
+       // let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
     }
 }
 
