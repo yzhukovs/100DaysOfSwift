a6ffc380d8c0bcc54c3f4b6a3d9a39a03295cae7
yzhukovs <yzhukovs@bumnetworks.com>
2019-10-29 14:42:57 -0400

    set up CLLocationManagerDelegate and CLLocationManager

--- project22/project22/ViewController.swift
+++ project22/project22/ViewController.swift
@@ -5,16 +5,31 @@
 //  Created by Yvette Zhukovsky on 10/28/19.
 //  Copyright © 2019 bumnetworks. All rights reserved.
 //
-
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        view.backgroundColor = .gray
     }
 
 
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    //do stuff
+                }
+            }
+        }
+    }
+    
 }
 
