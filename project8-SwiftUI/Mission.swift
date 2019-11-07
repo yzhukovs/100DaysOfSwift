39a8751ef713a2e12dd550665e61df8b1c38a46a
yzhukovs <yzhukovs@bumnetworks.com>
2019-11-06 22:42:29 -0500

    added Mission struct

---
+++ project8-SwiftUI/Mission.swift
@@ -0,0 +1,23 @@
+//
+//  Mission.swift
+//  project8-SwiftUI
+//
+//  Created by Yvette Zhukovsky on 11/6/19.
+//  Copyright © 2019 bumnetworks. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
