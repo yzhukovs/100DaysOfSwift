623512dcd6f7934cc76108454d173ebbf545f4b9
yzhukovs <yzhukovs@bumnetworks.com>
2019-11-06 22:50:30 -0500

    added mission.json

--- project8-SwiftUI/project8-SwiftUI/ContentView.swift
+++ project8-SwiftUI/project8-SwiftUI/ContentView.swift
@@ -9,9 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    let astronauts = Bundle.main.decode("astronauts.json")
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let mission: [Mission] = Bundle.main.decode("missions.json")
     var body: some View {
-        Text("\(astronauts.count)")
+        VStack {
+            Text("\(astronauts.count)")
+            Text("\(mission.count)")
+        }
     }
 }
 
