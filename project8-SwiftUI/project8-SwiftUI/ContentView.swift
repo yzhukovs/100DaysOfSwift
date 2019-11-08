15e816a320d8c086ca7757bb93d06c4457686492
yzhukovs <yzhukovs@bumnetworks.com>
2019-11-07 23:22:20 -0500

    fixed the error with different type names

--- project8-SwiftUI/project8-SwiftUI/ContentView.swift
+++ project8-SwiftUI/project8-SwiftUI/ContentView.swift
@@ -10,13 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
-    let mission: [Mission] = Bundle.main.decode("missions.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     var body: some View {
-        VStack {
-            Text("\(astronauts.count)")
-            Text("\(mission.count)")
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("DetailView")) {
+                    Image(mission.image)
+                
+            }
         }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- project8-SwiftUI/project8-SwiftUI/Mission.swift
+++ project8-SwiftUI/project8-SwiftUI/Mission.swift
@@ -20,4 +20,14 @@ struct Mission: Codable, Identifiable {
     let launchDate: String?
     let crew: [CrewRole]
     let description: String
+    
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
 }
