f3bd2bbca237341bb42a76c897f7755e20c29869
yzhukovs <yzhukovs@bumnetworks.com>
2019-11-21 16:35:09 -0500

    finished project

--- actual-project9-SwiftUI/actual-project9-SwiftUI/ContentView.swift
+++ actual-project9-SwiftUI/actual-project9-SwiftUI/ContentView.swift
@@ -28,12 +28,29 @@ struct Spirograph: Shape {
     }
     
     func path(in rect: CGRect) -> Path {
-        let path = Path()
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = CGFloat(self.outerRadius)
+        let innerRadius = CGFloat(self.innerRadius)
+        let distance = CGFloat(self.distance)
+        let difference = outerRadius - innerRadius
+        let endPoint = ceil(2 * CGFloat.pi * outerRadius / CGFloat(divisor)) * amount
         
         
         
-        
-        
+        var path = Path()
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+            x += rect.width / 2
+            y += rect.height / 2
+            
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            }else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+
         return path
     }
     
@@ -41,8 +58,43 @@ struct Spirograph: Shape {
 }
 
 struct ContentView: View {
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount: CGFloat = 1.0
+    @State private var hue = 0.6
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 0) {
+           Spacer()
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+            .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+            .frame(width: 300, height: 300)
+            Spacer()
+            
+            Group {
+                Text("Inner Radius \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 1...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Outer Radius \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 1...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Distance \(Int(distance))")
+                Slider(value: $distance, in: 1...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Amount \(amount, specifier: "%.2f")")
+                Slider(value: $amount)
+                .padding([.horizontal, .bottom])
+                
+                Text("Color")
+                Slider(value: $hue)
+                .padding(.horizontal)
+            }
+            
+        }
     }
 }
 
