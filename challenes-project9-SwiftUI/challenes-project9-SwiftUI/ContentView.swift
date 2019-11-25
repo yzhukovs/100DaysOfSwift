91011953481c755cdf9fd8a6e13b027a3ce7a61d
yzhukovs <yzhukovs@bumnetworks.com>
2019-11-24 21:34:20 -0500

    third chellenge - ColorCyclingRectangle added

--- challenes-project9-SwiftUI/challenes-project9-SwiftUI/ContentView.swift
+++ challenes-project9-SwiftUI/challenes-project9-SwiftUI/ContentView.swift
@@ -8,6 +8,38 @@
 
 import SwiftUI
 
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                .inset(by: CGFloat(value))
+                .strokeBorder(LinearGradient(gradient: Gradient(colors:[
+                    self.color(for: value, brightness: 1),
+                    self.color(for: value, brightness: 0.5)
+                ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+
+            }
+        }
+    .drawingGroup()
+    }
+    func color(for value:Int, brightness: Double ) -> Color {
+            var targetHue = Double(value) / Double(self.steps) + self.amount
+    
+            if targetHue > 1 {
+                targetHue -= 1
+            }
+    
+            return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    
+        }
+    
+    }
+
+
 struct Triangle: Shape {
     func path(in rect: CGRect)-> Path {
         var path = Path()
@@ -24,6 +56,7 @@ struct Triangle: Shape {
 
 struct ContentView: View {
     @State private var lineWidth: CGFloat = 10
+    @State private var colorCycle = 0.0
     var body: some View {
         VStack {
         Triangle()
@@ -56,6 +89,9 @@ struct ContentView: View {
             }
             }
             
+            ColorCyclingRectangle(amount: self.colorCycle)
+                            .frame(width: 100, height: 100)
+                       Slider(value: $colorCycle)
             
         }
         
