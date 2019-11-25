02db91f2c0ae17db0205503a9f9cf11f67d6f71a
yzhukovs <yzhukovs@bumnetworks.com>
2019-11-25 11:18:50 -0500

    project 10 swiftui setup

---
+++ project10-SwiftUI/project10-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project10-SwiftUI
+//
+//  Created by Yvette Zhukovsky on 11/25/19.
+//  Copyright © 2019 bumnetworks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
