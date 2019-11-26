4bb3c847bc3847289bbacf2c4538652ec59e775d
yzhukovs <yzhukovs@bumnetworks.com>
2019-11-26 15:21:40 -0500

    finished second challenge with whitespaces

--- project10-SwiftUI/project10-SwiftUI/Order.swift
+++ project10-SwiftUI/project10-SwiftUI/Order.swift
@@ -37,7 +37,8 @@ class Order: ObservableObject, Codable {
     
     
     var hasValidAddress: Bool {
-        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+        if name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+ || streetAddress.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || city.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || zip.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             return false
         }
         return true
