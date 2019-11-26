437e5b862b8550d694e757b280f78074a807ad13
yzhukovs <yzhukovs@bumnetworks.com>
2019-11-26 14:26:09 -0500

    adding networking urlsession code

--- project10-SwiftUI/project10-SwiftUI/CheckoutView.swift
+++ project10-SwiftUI/project10-SwiftUI/CheckoutView.swift
@@ -10,26 +10,56 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
     var body: some View {
         GeometryReader { geo in
             ScrollView {
                 VStack {
                     Image("cupcakes")
-                    .resizable()
-                    .scaledToFit()
+                        .resizable()
+                        .scaledToFit()
                         .frame(width: geo.size.width)
                     
                     Text("Your total is: $\(self.order.cost, specifier: "%2.f")")
                         .font(.title)
                     Button("Place order") {
                         //Place the order
+                        self.placeOrder()
                     }
-                .padding()
+                    .padding()
                 }
             }
         }
         .navigationBarTitle("Check Out", displayMode: .inline)
-}
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK!")))
+        }
+    }
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("failed to encode order")
+            return
+        }
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response\(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on it's way"
+            }
+            
+        }.resume()
+        
+    }
+    
 }
 struct CheckoutView_Previews: PreviewProvider {
     static var previews: some View {
