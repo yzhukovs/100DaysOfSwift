9b49498fe8d69f40e1bfb325f89ab7cc7f9ed10a
yzhukovs <yzhukovs@bumnetworks.com>
2019-11-30 22:43:56 -0500

    added core data, added review and addbookview

--- actual_project11-SwiftUI/actual_project11-SwiftUI/AddBookView.swift
+++ actual_project11-SwiftUI/actual_project11-SwiftUI/AddBookView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct AddBookView: View {
     @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
     @State private var title = ""
     @State private var author = ""
     @State private var rating = 3
@@ -19,7 +20,37 @@ struct AddBookView: View {
     let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id:\.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    RatingView(rating: $rating)
+                    TextField("Write a review", text: $review)
+                }
+                Section {
+                    Button("Save"){
+                        let newBook = Book(context: self.moc)
+                        newBook.title = self.title
+                        newBook.author = self.author
+                        newBook.rating = Int16(self.rating)
+                        newBook.genre = self.genre
+                        newBook.review = self.review
+                        try? self.moc.save()
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                }
+            }
+        .navigationBarTitle("Add Book")
+        }
     }
 }
 
--- actual_project11-SwiftUI/actual_project11-SwiftUI/ContentView.swift
+++ actual_project11-SwiftUI/actual_project11-SwiftUI/ContentView.swift
@@ -9,9 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    @State private var showingAddScreen = false
     
     var body: some View {
-        
+        NavigationView {
+            Text("Count: \(books.count)")
+            .navigationBarTitle("Bookworm")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showingAddScreen.toggle()
+                }) {
+                    Image(systemName: "plus")
+                })
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView().environment(\.managedObjectContext, self.moc)
+            }
+        }
     }
 }
 
