3db2f641f2d1da201f555f47f73e30fe721916cd
yzhukovs <yzhukovs@bumnetworks.com>
2019-12-11 23:59:07 -0500

    Added faceID

--- project14-SwiftUI-Bucketlist/project14-SwiftUI-Bucketlist/ContentView.swift
+++ project14-SwiftUI-Bucketlist/project14-SwiftUI-Bucketlist/ContentView.swift
@@ -16,9 +16,11 @@ struct ContentView: View {
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
     @State private var showingEditScreen = false
+    @State private var isUnlocked = false
     
     var body: some View {
         ZStack {
+            if isUnlocked {
             MapView(centerCoordinate: $centerCoordinate,selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
                 .edgesIgnoringSafeArea(.all)
                 Circle()
@@ -48,6 +50,16 @@ struct ContentView: View {
                 .clipShape(Circle())
                 .padding(.trailing)
                 }
+            }
+            } else {
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+                
             }
         }
         
@@ -86,6 +98,27 @@ struct ContentView: View {
             try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
         } catch {
             print("Unable to save data")
+        }
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+        let reason = "Please authentica yourself to unlock your places"
+        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success , authenticationError in
+            DispatchQueue.main.async {
+                if success {
+                    self.isUnlocked = true
+                } else {
+                    //error
+                }
+            
+            }
+        }
+        } else {
+            //no biometry
         }
     }
 }
