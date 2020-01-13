9ddd9e37123260bdcb5610c0568ac2a97011a997
yzhukovs <yzhukovs@bumnetworks.com>
2020-01-13 09:40:46 -0500

    added Image and QR code

--- project16-SwiftUI/project16-SwiftUI/MeView.swift
+++ project16-SwiftUI/project16-SwiftUI/MeView.swift
@@ -25,10 +25,30 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                     .padding([.horizontal, .bottom])
+                
+                Image(uiImage: generatQrCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    
                 Spacer()
+                
             }
         .navigationBarTitle("Your code")
         }
+    }
+    
+    func generatQrCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
